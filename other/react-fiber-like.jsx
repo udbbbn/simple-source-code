de9eea8b82eb7ef13999af945e5fbeb1f425afd0
udbbbn <udbbbn@163.com>
2021-09-10 15:59:16 +0800

    feat: react-fiber-like!! 简易代码不难.中断遍历的思想牛皮!

---
+++ other/react-fiber-like.jsx
@@ -0,0 +1,116 @@
+/*
+ * 本文内容直接 copy 于 https://juejin.cn/post/6925665796106485767
+ * 原文地址: https://indepth.dev/posts/1007/the-how-and-why-on-reacts-usage-of-linked-list-in-fiber-to-walk-the-components-tree
+ */
+
+// 使用sleep模拟渲染的耗费时间
+function sleep(ms = 100) {
+  let sleepSwitch = true;
+  let s = Date.now();
+  while (sleepSwitch) {
+    if (Date.now() - s > ms) {
+      sleepSwitch = false;
+    }
+  }
+}
+
+class Node {
+  constructor(instance) {
+    this.instance = instance;
+    this.child = null;
+    this.sibling = null;
+    this.return = null;
+  }
+}
+
+function link(parent, elements) {
+  if (elements === null) elements = [];
+  // 这里理论上应该使用 reduce 才对
+  // 使用 reduceRight 会导致 child 的倒序
+  parent.child = elements.reduceRight((previous, current) => {
+    const node = new Node(current);
+    node.return = parent;
+    node.sibling = previous;
+    return node;
+  }, null);
+  return parent.child;
+}
+
+// 节点
+const a1 = { name: "a1" };
+const b1 = { name: "b1" };
+const b2 = { name: "b2" };
+const b3 = { name: "b3" };
+const c1 = { name: "c1" };
+const c2 = { name: "c2" };
+const d1 = { name: "d1" };
+const d2 = { name: "d2" };
+a1.render = () => {
+  sleep(60);
+  return [b1, b2, b3];
+};
+b1.render = () => {
+  return [];
+};
+b2.render = () => {
+  sleep(20);
+  return [c1];
+};
+b3.render = () => {
+  sleep(20);
+  return [c2];
+};
+c1.render = () => {
+  sleep(40);
+  return [d1, d2];
+};
+c2.render = () => [];
+d1.render = () => [];
+d2.render = () => [];
+
+const root = new Node(a1);
+// 一直保持对当前节点的引用
+let current = root;
+// 是否渲染完成
+let isRendered = false;
+
+const rIcb = (deadline) => {
+  if (deadline.timeRemaining() > 20) {
+    walk(current, deadline);
+  } else {
+    requestIdleCallback(rIcb);
+  }
+};
+
+function doWork(node, deadline) {
+  if (deadline.timeRemaining() > 20) {
+    console.log(node.instance.name);
+    const children = node.instance.render();
+    return link(node, children);
+  } else {
+    console.log("抛到下一个空闲时间执行");
+    requestIdleCallback(rIcb);
+  }
+}
+
+function walk(o, deadline) {
+  while (true) {
+    let child = doWork(current, deadline);
+    if (child) {
+      current = child;
+      continue;
+    }
+    if (current === root) {
+      return;
+    }
+    while (!current.sibling) {
+      if (!current.return || current.return === root) {
+        return;
+      }
+      current = current.return;
+    }
+    current = current.sibling;
+  }
+}
+
+requestIdleCallback(rIcb);
