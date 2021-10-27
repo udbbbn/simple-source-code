a5b8fae35d5a40f1b1fc3ceac4b92c00c5a3ceef
udbbbn <udbbbn@163.com>
2021-10-28 00:15:01 +0800

    feat: 引入了组件状态

--- berial-like/index.js
+++ berial-like/index.js
@@ -1,10 +1,4 @@
-const apps = [];
-
-/* 此处做了 RAF 的兼容 但是在下一个 tag 版本中被删除 */
-const defer =
-  typeof requestAnimationFrame !== "undefined"
-    ? requestAnimationFrame
-    : setTimeout;
+const apps = []
 
 export function define(tag, component, route) {
   /**
@@ -12,43 +6,71 @@ export function define(tag, component, route) {
    * https://developer.mozilla.org/zh-CN/docs/Web/API/Window/customElements
    */
   class Berial extends HTMLElement {
+    static get name() {
+      return tag
+    }
+
     constructor() {
-      super();
-      const template = document.createElement("template");
-      template.innerHTML = `<!-- ${tag} -- ${route}  -->`;
-      this.attachShadow({ mode: "open" }).appendChild(template.cloneNode(true));
+      super()
+      /* 属性复制 */
+      for (const k in component) {
+        this[k] = component[k]
+      }
+      /* 自定义组件创建的代码被删除 意味着开发者必须在 html 文件中声明好 自定义组件并传入组件名 tag */
+    }
+
+    /* 自定义组件首次被插入 DOM 时触发 */
+    connectedCallback() {
+      this.attachShadow({ mode: "open" })
+
+      apps.push({
+        tag,
+        component,
+        route,
+        element: this,
+      })
+
+      invoke()
     }
   }
 
-  const hasDef = window.customElements.get(tag);
+  const hasDef = window.customElements.get(tag)
   if (!hasDef) {
-    window.customElements.define(tag, Berial);
+    window.customElements.define(tag, Berial)
   }
-
-  apps.push({
-    tag,
-    component,
-    route,
-  });
-
-  return invoke();
 }
 
 function invoke() {
-  /* 根据路由去加载组件 */
-  const path = window.location.hash || window.location.pathname;
-
-  apps.forEach((app) => {
-    defer(() => {
-      const host = document.querySelector(app.tag);
-      if (app.route === path) {
-        app.component.mount(host);
-      } else {
-        app.component.unmount(host);
-      }
-    });
-  });
+  /* 此处做了一个代理 对 对象的属性做修改时 触发 process (调用组件生命钩子) */
+  apps.forEach(app => {
+    /**
+     * 这里代理 app.element 因为 host 对象最终会传给 html 中的对象 会调用 shadowRoot
+     * 因为代理的缘故 最后从开发工具中也能从 app-one.count 中获取到值
+     */
+    const host = new Proxy(app.element, {
+      get(target, key) {
+        return target[key]
+      },
+      set(target, key, val) {
+        target[key] = val
+        process(app, host)
+        return true
+      },
+    })
+    process(app, host)
+  })
 }
 
-window.addEventListener("hashchange", invoke);
-window.addEventListener("popstate", invoke);
+function process(app, host) {
+  /* 根据路由去加载组件 */
+  const path = window.location.hash || window.location.pathname || "/"
+
+  if (app.route === path) {
+    app.component.mount(host)
+  } else {
+    app.component.unmount(host)
+  }
+}
+
+window.addEventListener("hashchange", invoke)
+window.addEventListener("popstate", invoke)
