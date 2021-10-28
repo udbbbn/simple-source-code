f25a3afa4e20d1bcc0b638689009e649de573ced
zhengzimin <udbbbn@gmail.com>
2021-10-28 18:10:58 +0800

    chore: 更换rollup ts插件-生成d.ts & 修改输出 umd & es 两种格式

--- berial-like/rollup.config.js
+++ berial-like/rollup.config.js
@@ -1,12 +1,20 @@
-import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/berial.js',
-    format: 'umd',
-    sourcemap: true,
-    name: 'berial',
-  },
+  output: [
+    {
+      file: 'dist/es/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+      name: 'berial',
+    },
+    {
+      file: 'dist/umd/index.js',
+      format: 'umd',
+      sourcemap: true,
+      name: 'berial',
+    },
+  ],
   plugins: [typescript()],
 }
