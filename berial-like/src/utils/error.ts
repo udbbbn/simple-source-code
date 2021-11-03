cbbe0f43c86d31f3ffbd466384194a819b01b01d
zhengzimin <udbbbn@gmail.com>
2021-11-03 10:37:10 +0800

    feat: 完善 error 类型

--- berial-like/src/utils/error.ts
+++ berial-like/src/utils/error.ts
@@ -1,10 +1,16 @@
-export function warn(trigger: string | boolean, msg?: string) {
+export function warn(trigger: boolean): void
+export function warn(trigger: string): void | Error
+export function warn(trigger: boolean, msg?: string): void | Error
+export function warn(trigger: boolean | string, msg?: string) {
   if (typeof trigger === 'string') msg = trigger
   if (!trigger) return
   throw new Error(`[Berial-Like Warning]: ${msg}`)
 }
 
-export function error(trigger: string | boolean, msg?: string) {
+export function error(trigger: boolean): void
+export function error(trigger: string): void | Error
+export function error(trigger: boolean, msg?: string): void | Error
+export function error(trigger: boolean | string, msg?: string) {
   if (typeof trigger === 'string') msg = trigger
   if (!trigger) return
   throw new Error(`[Berial-Like Error]: ${msg}`)
