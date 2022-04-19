2bcfea2c18315829bb57579e8a116685cd29071c
udbbbn <udbbbn@163.com>
2022-04-19 16:13:52 +0800

    feat: specific render method & variable definition

--- react-draggable-tag/src/App.tsx
+++ react-draggable-tag/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Draggable from "./draggable";
 
 const initialTags = [
--- react-draggable-tag/src/draggable.tsx
+++ react-draggable-tag/src/draggable.tsx
@@ -1,24 +1,129 @@
-import { useEffect, useState } from "react";
+import { CSSProperties } from "react";
+import { useEffect, useRef, useState } from "react";
+import "./index.less";
 
-type Id = number | string | Symbol;
-type Tag = { id: Id; content: string };
-type renderParams = {
+type Id = number | string;
+type Tag = {
+  id: Id;
+  content: string;
+  unDraggable?: boolean;
+  [otherProps: string]: any;
+};
+type Position = {
+  id: Id;
+  left: number;
+  top: number;
+  bottom: number;
+  right: number;
+  width: number;
+  height: number;
+}[];
+
+type RenderParams = {
   tag: Tag;
   index: number;
+};
+type DomCtx = {
+  draggbleWrapper: HTMLDivElement;
+  tagEles: { [key: Id]: HTMLDivElement };
+  tagPositions: Position;
+  draggbleTagEles: { [key: Id]: HTMLDivElement };
 };
 
 interface Props {
   tags?: Tag[];
   initialTags?: Tag[];
-  render: (renderParams) => JSX.Element;
+  className?: string;
+  tagStyle?: CSSProperties;
+  render: (params: RenderParams) => JSX.Element;
   onChange: (tag: JSX.Element) => void;
 }
 
 export default function Draggable(props: Props) {
-  const { initialTags } = props;
-  const [tag, setTag] = useState([]);
+  const {
+    initialTags,
+    className,
+    tagStyle = {},
+    tags: propTags,
+    render: propRender,
+  } = props;
+  const [tags, setTags] = useState<Tag[]>([]);
+  const domCtx = useRef<DomCtx>({
+    draggbleWrapper: null,
+    tagEles: {},
+    tagPositions: [],
+    draggbleTagEles: {},
+  });
 
-  useEffect(() => {}, [initialTags]);
+  useEffect(() => {
+    if (initialTags && initialTags.length) {
+      setTags(initialTags);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return <div></div>;
+  useEffect(() => {
+    if (
+      propTags.length !== tags.length ||
+      propTags.some((el, i) => el.id !== tags[i].id)
+    ) {
+      setTags(propTags);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [propTags]);
+
+  useEffect(() => {
+    domCtx.current.tagPositions = [];
+    tags.forEach((t) => {
+      const { draggbleTagEles, tagEles, tagPositions } = domCtx.current;
+      const domTarget = draggbleTagEles[t.id];
+      const tag = tagEles[t.id];
+      tagPositions.push({
+        id: t.id,
+        left: tag.offsetLeft,
+        top: tag.offsetTop,
+        bottom: tag.offsetTop + tag.offsetHeight,
+        right: tag.offsetLeft + tag.offsetWidth,
+        width: tag.offsetWidth,
+        height: tag.offsetHeight,
+      });
+      if (!t.unDraggable) {
+        dragElement(tag, t.id, domTarget);
+      }
+    });
+    console.log("result", domCtx.current);
+  }, [tags]);
+
+  function dragElement(ele, id, parent) {}
+
+  return (
+    <div
+      className={`DraggbleWrapper ${className || ""}`}
+      ref={(v) => (domCtx.current.draggbleWrapper = v)}
+    >
+      {tags.map((tag, i) => (
+        <div
+          key={tag.id}
+          className={"DraggbleWrapper-tag"}
+          style={tagStyle}
+          ref={(v) => (domCtx.current.tagEles[tag.id] = v)}
+        >
+          <div
+            className={"DraggbleWrapper-tag-drag"}
+            ref={(v) => (domCtx.current.draggbleTagEles[tag.id] = v)}
+          >
+            {propRender({ tag, index: i })}
+          </div>
+          <div
+            style={{
+              opacity: 0,
+              overflow: "hidden",
+            }}
+          >
+            {propRender({ tag, index: i })}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
